0c7bdff9ab0d5f7bc573e91f1fea92c2efc1b299
Gerin_Befis <120281303+Maciekhidegger@users.noreply.github.com>
2024-02-26 21:05:11 +0100

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+// RANDOM_HENTAI by SecretumAnglicum
+
+int main() { 
+
+	fstream file;
+
+	int numerator=1;
+
+	string *file_text;
+
+	 file_text = new string [1000];
+	string line, *indicator=file_text;
+
+
+
+	//RANDOMIZE
+	file.open("HENTAI_BASE.txt", ios::in);
+
+	while(!file.eof()) {
+	getline(file, line);
+	*indicator=line;
+		indicator++; numerator++;
+
+	}
+
+	file.close(); 
+
+	srand(time(NULL));
+
+	int random=rand()%numerator;
+
+	for(int i=random; i>=1; i--) {
+	indicator--; 
+	}
+
+	system("clear");
+	cout << "HENTAI TODAY: " << *indicator << endl; getchar();
+
+	delete [] file_text;
+
+	return 0;
+}
